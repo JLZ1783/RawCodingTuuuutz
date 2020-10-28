6ea7a58aaab37f9f077172a9ad2db268cbbcdccd
JLZ1783 <50119881+JLZ1783@users.noreply.github.com>
2020-10-27 21:16:41 -0400

    Initial commit

---
+++ TrickingLibraryApi/Controllers/Home.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibraryApi.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class Home: ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World";
+
+        }
+    }
+}
